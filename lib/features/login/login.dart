1c6bb718b0e0599481ff0d36562e586300368da4
abdullah mohamed <abdullahmohamed1122002.com>
2024-09-22 21:39:29 +0300

    new t

--- lib/features/login/login.dart
+++ lib/features/login/login.dart
@@ -5,6 +5,6 @@ class LogInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('log in screen ffffvffggffgffffvvvv'),);
+    return Scaffold(body: Text('log in screen t'),);
   }
 }
