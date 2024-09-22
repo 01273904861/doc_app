c4cc06563f6c41f1e099546f15e68a136bd3e1cd
abdullah mohamed <abdullahmohamed1122002.com>
2024-09-22 22:28:32 +0300

    container in login screen

--- lib/features/login/login.dart
+++ lib/features/login/login.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LogInScreen extends StatelessWidget {
   const LogInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('log in screen tttttt'),);
+    return Scaffold(body: Container(
+      width: 300.w,
+      color:Colors.blue,
+      child: const Text('log in screen')),);
   }
 }
