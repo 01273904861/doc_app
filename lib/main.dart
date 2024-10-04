395fac5fd94493493d5ac952ca44fd7068ca9e1a
abdullah mohamed <abdullahmohamed1122002.com>
2024-10-04 04:18:04 +0300

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:appoment_app/core/di/dependency_injection.dart';
 import 'package:appoment_app/core/routings/app_router.dart';
 import 'package:appoment_app/features/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setUpGetIt();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
