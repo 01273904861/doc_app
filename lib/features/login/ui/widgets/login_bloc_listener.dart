ecc68265d33ac9fb58d41c6f32bc310a7ea95a73
abdullah mohamed <abdullahmohamed1122002.com>
2024-11-15 23:10:09 +0200

    ff

--- lib/features/login/ui/widgets/login_bloc_listener.dart
+++ lib/features/login/ui/widgets/login_bloc_listener.dart
@@ -5,7 +5,6 @@ import 'package:appoment_app/features/login/logic/cubit/login_cubit.dart';
 import 'package:appoment_app/features/login/logic/cubit/login_states.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginBlocListener extends StatelessWidget {
   const LoginBlocListener({super.key});
